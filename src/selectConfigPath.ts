11f5f29056bbd5ac829f5284f444f1558054b319
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2024-01-28 09:40:55 +0900

    add comment

--- src/selectConfigPath.ts
+++ src/selectConfigPath.ts
@@ -1,5 +1,12 @@
 import * as vscode from "vscode";
 
+/**
+ * Prompts the user to select a configuration file path from the available options.
+ * If a path is selected, it updates the global state with the selected path and displays an information message.
+ * If no configuration paths are defined, it displays a message indicating that no paths are defined.
+ *
+ * @param context The extension context.
+ */
 export function selectConfigPath(context: vscode.ExtensionContext) {
   const configPaths: string[] | undefined = vscode.workspace
     .getConfiguration()
