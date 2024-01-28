01f00c42db0f7fbcf4577fa68f731689871f1c6b
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2024-01-28 09:46:26 +0900

    add comment

--- src/ScenarigoCodeLensProvider.ts
+++ src/ScenarigoCodeLensProvider.ts
@@ -1,6 +1,12 @@
 import * as vscode from "vscode";
 
 export class ScenarigoCodeLensProvider implements vscode.CodeLensProvider {
+  /**
+   * A single CodeLens is provided at the top of the document allowing the user to run scenarigo tests.
+   *
+   * @param document The document for which CodeLenses should be provided.
+   * @returns An array of CodeLens objects or a promise that resolves to an array of CodeLens objects.
+   */
   public provideCodeLenses(
     document: vscode.TextDocument
   ): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
