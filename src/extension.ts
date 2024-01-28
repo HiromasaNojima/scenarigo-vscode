49b341b6c4d1581c400cd63a2a91e216d9549f90
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2024-01-28 09:08:53 +0900

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,26 @@
 import * as vscode from "vscode";
-import { spawn } from "child_process";
 import { ScenarigoCodeLensProvider } from "./scenarigoCodeLensProvider";
 import { selectConfigPath } from "./selectConfigPath";
+import { ScenarigoRunner } from "./scenarigoRunner";
 
 export function activate(context: vscode.ExtensionContext) {
-  const runScenarigoCommand = vscode.commands.registerCommand(
-    "scenarigo-vscode.runScenarigo",
-    (filePath) => {
-      if (filePath) {
-        // run from code lens
-        runScenarigo(context, filePath);
-        console.log("Scenarigo command executed");
-      } else {
-        // run from command palette
-        const activeEditor = vscode.window.activeTextEditor;
-        if (activeEditor && activeEditor.document.languageId === "yaml") {
-          runScenarigo(context, activeEditor.document.uri.fsPath);
+  const runner = new ScenarigoRunner(context);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "scenarigo-vscode.runScenarigo",
+      (filePath) => {
+        if (filePath) {
+          // run from code lens
+          runner.runScenarigo(filePath.fsPath);
+        } else {
+          // run from command palette
+          const activeEditor = vscode.window.activeTextEditor;
+          if (activeEditor && activeEditor.document.languageId === "yaml") {
+            runner.runScenarigo(activeEditor.document.uri.fsPath);
+          }
         }
       }
-    }
+    )
   );
 
   const selectConfigPathCommand = vscode.commands.registerCommand(
@@ -27,7 +29,6 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   const provider = new ScenarigoCodeLensProvider();
-  context.subscriptions.push(runScenarigoCommand);
   context.subscriptions.push(selectConfigPathCommand);
   context.subscriptions.push(
     vscode.languages.registerCodeLensProvider({ language: "yaml" }, provider)
@@ -35,68 +36,3 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {}
-
-async function runScenarigo(
-  context: vscode.ExtensionContext,
-  filePath: string
-) {
-  const selectedConfigPath =
-    context.globalState.get<string>("selectedConfigPath");
-
-  if (!selectedConfigPath) {
-    selectConfigPath(context);
-    return;
-  }
-
-  runCommandAndAppendToPanel(filePath, selectedConfigPath);
-}
-
-let outputPanel: vscode.WebviewPanel | undefined;
-
-function createOutputPanel() {
-  // 既にパネルが存在する場合はそれを使用
-  if (outputPanel) {
-    outputPanel.reveal(vscode.ViewColumn.Two);
-    return outputPanel;
-  }
-
-  // 新しい Webview Panel を作成
-  outputPanel = vscode.window.createWebviewPanel(
-    "outputPanel", // 識別子
-    "Command Output", // パネルのタイトル
-    vscode.ViewColumn.Two // パネルを表示するカラム（Two は右側を意味する）
-  );
-
-  // パネルが閉じられたときの処理
-  outputPanel.onDidDispose(() => {
-    outputPanel = undefined;
-  }, null);
-
-  return outputPanel;
-}
-
-function appendToOutputPanel(message: string) {
-  const panel = createOutputPanel();
-  const content = `<pre>${message}</pre>`;
-  panel.webview.html += content;
-}
-
-// この関数をコマンドの実行結果を表示するために使用する
-function runCommandAndAppendToPanel(
-  filePath: string,
-  selectedConfigPath: string
-) {
-  const child = spawn("scenarigo", ["run", filePath, "-c", selectedConfigPath]);
-
-  child.stdout.on("data", (data) => {
-    appendToOutputPanel(data.toString());
-  });
-
-  child.stderr.on("data", (data) => {
-    appendToOutputPanel(data.toString());
-  });
-
-  child.on("close", (code) => {
-    appendToOutputPanel(`\nProcess exited with code ${code}`);
-  });
-}
---
+++ src/scenarigoRunner.ts
@@ -0,0 +1,70 @@
+import * as vscode from "vscode";
+import { spawn } from "child_process";
+
+const messageSetConfigPath = `No configuration path is set. \nSet one from the command palette "Scenarigo: Select Config Path".`;
+
+export class ScenarigoRunner {
+  private outputPanel: vscode.WebviewPanel | undefined;
+
+  constructor(private context: vscode.ExtensionContext) {}
+
+  public runScenarigo(filePath: string) {
+    const selectedConfigPath =
+      this.context.globalState.get<string>("selectedConfigPath");
+    if (!selectedConfigPath) {
+      this.appendToOutputPanel(messageSetConfigPath);
+      return;
+    }
+
+    this.runCommandAndAppendToPanel(filePath, selectedConfigPath);
+  }
+
+  private createOutputPanel() {
+    if (this.outputPanel) {
+      this.outputPanel.reveal(vscode.ViewColumn.Two);
+      return this.outputPanel;
+    }
+
+    this.outputPanel = vscode.window.createWebviewPanel(
+      "outputPanel",
+      "Command Output",
+      vscode.ViewColumn.Two
+    );
+
+    this.outputPanel.onDidDispose(() => {
+      this.outputPanel = undefined;
+    }, null);
+
+    return this.outputPanel;
+  }
+
+  private appendToOutputPanel(message: string) {
+    const panel = this.createOutputPanel();
+    const content = `<pre>${message}</pre>`;
+    panel.webview.html += content;
+  }
+
+  private runCommandAndAppendToPanel(
+    filePath: string,
+    selectedConfigPath: string
+  ) {
+    const child = spawn("scenarigo", [
+      "run",
+      filePath,
+      "-c",
+      selectedConfigPath,
+    ]);
+
+    child.stdout.on("data", (data) => {
+      this.appendToOutputPanel(data.toString());
+    });
+
+    child.stderr.on("data", (data) => {
+      this.appendToOutputPanel(data.toString());
+    });
+
+    child.on("close", (code) => {
+      this.appendToOutputPanel(`\nProcess exited with code ${code}`);
+    });
+  }
+}
